2ccdd207ef49a17d4a606dcb91cde15027d6b242
Ruth <aylaaeternum@gmail.com>
2022-02-12 20:31:56 +0100

    style updates

---
+++ js/my-js.js
@@ -0,0 +1,10 @@
+var btnTop = document.getElementById("btn-top");
+var logo = document.getElementById("logo");
+
+btnTop.addEventListener("click", function () {
+    window.scrollTo(0, 0);
+});
+
+logo.addEventListener("click", function () {
+    window.scrollTo(0, 0);
+});
