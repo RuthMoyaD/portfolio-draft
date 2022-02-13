128d309baa72d934416a351ede2d49a2aa23645c
Ruth <aylaaeternum@gmail.com>
2022-02-13 08:57:22 +0100

    js updates

--- js/my-js.js
+++ js/my-js.js
@@ -2,9 +2,9 @@ var btnTop = document.getElementById("btn-top");
 var logo = document.getElementById("logo");
 
 btnTop.addEventListener("click", function () {
-    window.scrollTo(0, 0);
+    window.scrollTo({ top: 0, behavior: 'smooth' });
 });
 
 logo.addEventListener("click", function () {
-    window.scrollTo(0, 0);
+    window.scrollTo({ top: 0, behavior: 'smooth' });
 });
